a2d1250ebf8e282b26175c6af00f7fe4a6c4948d
Sudoharry <harendrabarot@gmail.com>
2025-01-12 17:31:50 +0530

    Added the project with rt association pending

--- Basic-Practice/Terraform-module/main.tf
+++ Basic-Practice/Terraform-module/main.tf
@@ -17,14 +17,14 @@ module "aws_subnet" {
 
 module "igw_module" {
  source = "./modules/aws_igw"
- for_each = var.igw_config
+ ## for_each = var.igw_config
  vpc_id = module.vpc_modules[each.value.vpc_name].vpc_id
  tags = each.value.tags
 }
 
 module "NatGW_module" {
   source = "./modules/aws_natGW"
-  for_each = var.nat_GW_config
+  # for_each = var.nat_GW_config
   elasticIP_id = module.elastic_IP_module[each.value.eip_name].elastic_IP_id
   subnet_id = module.aws_subnet[each.value.subnet_name].subnet_id
   tags = each.value.tags
@@ -32,13 +32,13 @@ module "NatGW_module" {
 
 module "elastic_IP_module" {
   source = "./modules/aws_elastic_IP"
-  for_each = var.elastic_IP_config
+  ## for_each = var.elastic_IP_config
   tags = each.value.tags
 }
 
 module "route_table_module" {
   source = "./modules/aws_route_table"
-  for_each = var.route_table_config
+  ## for_each = var.route_table_config
   vpc_id = module.vpc_modules[each.value.vpc_name].vpc_id
   igw_id = each.value.private == 0 ? module.igw_module[each.value.gatewat_name].igw_id : module.nat_GW_config[each.value.gatewat_name].nat_GW_id
   tags = each.value.tags
@@ -46,7 +46,7 @@ module "route_table_module" {
 
 module "route_table_association" {
   source = "./modules/aws_rt_association"
-  for_each = var.aws_rt_association_config
+  ### for_each = var.aws_rt_association_config
   subnet_id = module.aws_subnet[each.value.subnet_name].subnet_id
   route_table_id = module.route_table_module[each.value.route_table_name].route_table_id
 }
